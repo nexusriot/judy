fcafac0f191db9b9e6d0b673f108af12bc88befc
Nexus <nexus.riot@gmail.com>
2021-09-23 15:45:05 +0300

    Moved client and server to separate structs

---
+++ client/client.go
@@ -0,0 +1,156 @@
+package client
+
+import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+
+	pb "github.com/nexusriot/judy/proto"
+)
+
+const idFile = "judy.dat"
+
+type Client struct {
+	Id      string
+	Client  *http.Client
+	Logger  *zap.Logger
+	Session string
+}
+
+//func (c *Client) execCommand(command string) (string, error) {
+//	cmd := exec.Command(command)
+//	//cmd.Stdin = strings.NewReader()
+//	var out bytes.Buffer
+//	cmd.Stdin = &out
+//	err := cmd.Run()
+//	if err != nil {
+//		c.Logger.Error("Failed to execute command", zap.Error(err))
+//		return "", err
+//	}
+//	output := out.String()
+//	c.Logger.Info("output", zap.String("output", output))
+//	return output, err
+//}
+
+func (c *Client) readClientId() (string, error) {
+	file, err := os.Open(idFile)
+	if err != nil {
+		return "", err
+	}
+	stats, statsErr := file.Stat()
+	if statsErr != nil {
+		return "", statsErr
+	}
+
+	var size = stats.Size()
+	bytes := make([]byte, size)
+
+	buf := bufio.NewReader(file)
+	_, err = buf.Read(bytes)
+	id, err := uuid.FromBytes(bytes)
+	if err != nil {
+		c.Logger.Fatal("Couldn't parse uuid")
+		file.Close()
+		_ = os.Remove(idFile)
+		return "", err
+	}
+	file.Close()
+	return id.String(), err
+}
+
+func (c *Client) generateClientId() string {
+	c.Logger.Debug("Generating new client id")
+	f, err := os.Create(idFile)
+	defer f.Close()
+	if err != nil {
+		c.Logger.Fatal("Couldn't create id file")
+	}
+	idBytes, err := uuid.New().MarshalBinary()
+	w := bufio.NewWriter(f)
+	_, err = w.Write(idBytes)
+	if err != nil {
+		c.Logger.Fatal("Couldn't write id file")
+	}
+	err = w.Flush()
+	if err != nil {
+		c.Logger.Fatal("Couldn't write id file")
+	}
+	id, err := uuid.FromBytes(idBytes)
+	if err != nil {
+		c.Logger.Fatal("Couldn't parse uuid")
+	}
+	return id.String()
+}
+
+func (c *Client) getClientId() (string, error) {
+	res, err := c.readClientId()
+	if os.IsNotExist(err) {
+		c.Logger.Warn("client id not found")
+		res = c.generateClientId()
+	}
+	return res, err
+}
+
+func (c *Client) Heartbeat() {
+	hb := pb.Heartbeat{ClientId: c.Id}
+	data, err := proto.Marshal(&hb)
+	responseBody := bytes.NewBuffer(data)
+	req, err := http.NewRequest(http.MethodPost, "http://localhost:1337/hb", responseBody)
+	if err != nil {
+		//  TODO:
+	}
+	req.Header.Set("User-Agent", "ncl")
+	req.Header.Set("content-type", "application/x-protobuf")
+	res, getErr := c.Client.Do(req)
+	if getErr != nil {
+		// TODO:
+	}
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		// TODO:
+	}
+	hbResponse := new(pb.HeartbeatResponse)
+	err = proto.Unmarshal(body, hbResponse)
+	if err != nil {
+		c.Logger.Error("Failed to unmarshal heartbeat", zap.Error(err))
+	}
+	c.Logger.Info(hbResponse.ClientId)
+}
+
+func (c *Client) Run() {
+	client_id, err := c.getClientId()
+	if err != nil {
+		c.Logger.Panic("Failed to get client id")
+	}
+	c.Id = client_id
+	c.Logger.Info("Client id %s", zap.String("client_id", client_id))
+	for {
+		c.Heartbeat()
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func NewClient() (*Client, error) {
+	var loggerConfig = zap.NewProductionConfig()
+	loggerConfig.Level.SetLevel(zap.DebugLevel)
+	logger, err := loggerConfig.Build()
+	if nil != err {
+		return nil, err
+	}
+	logger.Info("Starting client")
+
+	client := http.Client{
+		Timeout: time.Second * 10,
+	}
+	return &Client{Client: &client, Logger: logger}, nil
+}
---
+++ server/server.go
@@ -0,0 +1,56 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/golang/protobuf/proto"
+	"go.uber.org/zap"
+
+	"github.com/nexusriot/judy/pkg/db"
+	pb "github.com/nexusriot/judy/proto"
+)
+
+type JudyServer struct {
+	db     *db.JudyDb
+	logger *zap.Logger
+	engine *gin.Engine
+}
+
+func NewJudyServer() (*JudyServer, error) {
+	var loggerConfig = zap.NewProductionConfig()
+	loggerConfig.Level.SetLevel(zap.DebugLevel)
+	logger, err := loggerConfig.Build()
+	if nil != err {
+		return nil, err
+	}
+	judyDB := db.NewJudyDb(true, logger)
+	r := gin.Default()
+	return &JudyServer{
+		db:     judyDB,
+		logger: logger,
+		engine: r,
+	}, nil
+}
+
+func (s *JudyServer) Run(address string) {
+	defer s.db.Close()
+	s.engine.POST("/hb", func(c *gin.Context) {
+		hbResponse := s.handleHeartbeat(c)
+		c.ProtoBuf(200, hbResponse)
+	})
+	s.engine.Run(address)
+}
+
+func (s *JudyServer) handleHeartbeat(c *gin.Context) *pb.HeartbeatResponse {
+	body, _ := c.GetRawData()
+	hb := new(pb.Heartbeat)
+	err := proto.Unmarshal(body, hb)
+	if err != nil {
+		s.logger.Error("Failed to unmarshal heartbeat", zap.Error(err))
+	}
+	s.logger.Info("Received Heartbeat from", zap.String("client_id", hb.ClientId))
+	s.db.AddClient(hb.ClientId, c.ClientIP())
+	// TODO: collect tasks
+	hbResponse := new(pb.HeartbeatResponse)
+	hbResponse.ClientId = hb.ClientId
+	return hbResponse
+}
